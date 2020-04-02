91e03d72c9ba82a0dfd539cd14eaae1a073080fa
Luiz Adamchuk <adis_jr@hotmail.com>
2020-04-02 13:26:41 -0300

    Upload Backend Test

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require('cors')
+const requireDir = require('require-dir')
+
+const app = express()
+//Permite que o express entenda a linguagem json
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://adamchuk:adamchuk@cluster0-gfnun.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+requireDir('./src/models')
+
+
+app.use('/api', require("./src/routes"))
+
+app.listen(3001)
